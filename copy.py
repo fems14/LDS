e7f84f06f1931dca7448b2ec3044dc6bf536cf51
fems14 <74094523+fems14@users.noreply.github.com>
2025-06-12 19:33:44 +0800

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,2 +1,4 @@
                 max_context_chunk = round_down(max_context_chunk,
                                                self.block_size)
+        self.chunked_prefill_for_mla = additional_config.get(
+            "chunked_prefill_for_mla", False)
